e0c38867df5db88024ece0e99790854c29f1c206
Garrett <grt002@outlook.com>
2023-02-20 14:23:38 -0600

    added more functionality to the script.js file

--- script.js
+++ script.js
@@ -16,19 +16,39 @@ generateBtn.addEventListener("click", writePassword);
 // NEW - Generate a password that meets the user's selected criteria
 function generatePassword {
 
-  // NEW - Array definitions for each character type
-  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numbericChars = "0123456789";
-  var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*'. '('. ')'. '-', '_', '+', '+', '+', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', ',', '.', '?'];
-
-  // NEW - Prompt for password length
+  // NEW - Prompt for the password length
   var passwordLength = prompt("Enter password length (must be between 8 and 128 characters):");
 
-  // NEW - Validate password length
+  // NEW - Validate the password length
   while (passwordLength < 8 || passwordLength > 128) {
     passwordLength = prompt("Invalid length. Please enter a length between 8 and 128 characters:");
   }
 
+  // NEW - Array definitions for each character type
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = ['!', '@', '#', '$', '%', '^', '&', '*'. '('. ')'. '-', '_', '+', '+', '+', '{', '}', '[', ']', '|', ':', ';', '"', '<', '>', ',', '.', '?'];
+
+  // NEW - prompt for character types to include in the password
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeUppercase = confirm("Include uppercase letters>");
+  var includeNumbers = confirm("Include numbers?");
+  var includeSpecialChars = confirm("Include special characters?");
+
+  // NEW - Validate that at least one character type is selected
+  while (!includeLowercase && !includeUppercase && !includeNumbers && !includeSpecialChars) {
+    alert("Please select at least one character type.");
+    includeLowercase = confirm("Include lowercase letters?");
+    includeUppercase = confirm("Include uppercase letters?");
+    includeNumbers = confirm("Include numbers?");
+    includeSpecialChars = confirm("Include special characters?");
+  }
+
+  //NEW - Initialize the password string
+  var password = '';
+
   // Other code for generating the password
+
+  return password;
 }
