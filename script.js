d9b3771b17d3c9ca08b6a6dbe2b1740548e7dd37
Garrett <grt002@outlook.com>
2023-02-20 13:43:47 -0600

    Added a function to generate password to script.js

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -15,3 +12,14 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// NEW - Generate a password that meets the user's selected criteria
+function generatePassword {
+  // Prompt for password length
+  var passwordLength = prompt("Enter password length (must be between 8 and 128 characters):");
+  // Validate password length
+  while (passwordLength < 8 || passwordLength > 128) {
+    passwordLength = prompt("Invalid length. Please enter a length between 8 and 128 characters:");
+  }
+  // Other code for generating the password
+}
